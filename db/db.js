583c3298a9c0dc2983e60f6d91809607cace206f
Harit Choudhary <harit.choudhary.bct.2023@gmail.com>
2026-05-04 13:53:09 +0530

    minor fixes

--- db/db.js
+++ db/db.js
@@ -4,7 +4,18 @@ let isConnected = false;
 
 async function dbConnect() {
     try {
-        const db = await mongoose.connect("mongodb://localhost:27017/students");
+
+        if (isConnected) {
+            console.log("Already connected to the database");
+            return;
+        }
+
+        const db = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        isConnected = db.connections[0].readyState;
 
         console.log("Database connected");
     } catch (error) {
